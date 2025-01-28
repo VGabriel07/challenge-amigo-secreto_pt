2012fc8272e70f9aac7757f1c98b6ab1820ff811
VGabriel <victorbianchini419@gmail.com>
2025-01-28 18:56:23 -0300

    Subir desafio Amigo Secreto

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let nomeAmigo;
+
+function adicionarAmigo() {
+    nomeAmigo = document.getElementById("amigo").value;
+    if (nomeAmigo === "") {
+        alert("Por favor, insira um nome");
+        return;
+    }
+    amigos.push(nomeAmigo);
+    exibirListaDeAmigos();
+    limparInput();
+}
+
+function limparInput() { // limpa o campo de inserir os nomes
+    nomeAmigo = document.getElementById("amigo");
+    nomeAmigo.value = "";
+}
+
+function exibirListaDeAmigos() {
+    let listaDeAmigos = document.getElementById("listaAmigos"); 
+    listaDeAmigos.innerHTML = ""; // Limpa a lista
+
+    for (let nomeDoAmigo of amigos) { 
+        let itemDaLista = document.createElement("li"); 
+        itemDaLista.textContent = nomeDoAmigo;
+        listaDeAmigos.appendChild(itemDaLista);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Não há amigos na lista! Adicione amigos para realizar o sorteio.");
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    const resultadoLista = document.getElementById("resultado");
+    resultadoLista.innerHTML = `O amigo sorteado é: ${amigoSorteado}`;
+}
+
+function limparLista() { // Limpa os nomes adicionados na lista
+    amigos = [];
+    let limparListaAmigos = document.getElementById("listaAmigos");
+    limparListaAmigos.innerHTML = "";
+    const resultadoLista = document.getElementById("resultado");
+    resultadoLista.innerHTML = "";
+}
+
+
+
